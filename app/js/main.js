7d9aa25c709b2e2a5b1fd218fcabfe698e91f3d0
Valentin3298 <zloy_aX@fmail.com>
2020-07-20 20:36:13 +0300

    New-items first

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,27 @@
 $(function () {
-
-  var mixer = mixitup(".products__inner-box");
-  
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "12px",
     ratedFill: "#ffa726",
-    readOnly: true
+    readOnly: true,
   });
-
+  
   $(".product-slider__inner").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     autoplay: true,
     autoplaySpeed: 3000,
     arrows: false,
-    dots: true
-  })
-
+    dots: true,
+  });
+  
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
+  var mixer = mixitup(".products__inner-box");
 });
