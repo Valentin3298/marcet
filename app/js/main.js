b953cc7e93ba9705379e573fcf9fdee81bb40cea
Valentin3298 <zloy_aX@fmail.com>
2020-07-21 18:40:26 +0300

    added items-page

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,7 @@ $(function () {
     ratedFill: "#ffa726",
     readOnly: true,
   });
-  
+
   $(".product-slider__inner").slick({
     slidesToShow: 4,
     slidesToScroll: 4,
@@ -14,14 +14,26 @@ $(function () {
     arrows: false,
     dots: true,
   });
-  
+
   $(".js-range-slider").ionRangeSlider({
     type: "double",
     min: 0,
     max: 1000,
     from: 0,
     to: 600,
-    prefix: "$"
+    prefix: "$",
   });
+
+  $(".icon-th-list").on("click", function () {
+    $(".products__item").addClass("list");
+    $(".icon-th-list").addClass("active");
+    $(".icon-th-large").removeClass("active");
+  });
+  $(".icon-th-large").on("click", function () {
+    $(".products__item").removeClass("list");
+    $(".icon-th-large").addClass("active");
+    $(".icon-th-list").removeClass("active");
+  });
+
   var mixer = mixitup(".products__inner-box");
 });
