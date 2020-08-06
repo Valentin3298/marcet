b3235e0aaeccabf402062274219eaca91d6794b8
Valentin3298 <zloy_aX@fmail.com>
2020-08-06 20:15:35 +0300

    adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,31 @@ $(function () {
     autoplaySpeed: 3000,
     arrows: false,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   $(".js-range-slider").ionRangeSlider({
@@ -24,10 +49,17 @@ $(function () {
     prefix: "$",
   });
 
-  $(".product-one__tabs .tab, .settings__tabs .tab").on("click", function (event) {
+  $(".product-one__tabs .tab, .settings__tabs .tab").on("click", function (
+    event
+  ) {
     var id = $(this).attr("data-id");
-    $(".product-one__tabs, .settings__tabs").find(".tab-item").removeClass("active-tab").hide();
-    $(".product-one__tabs .tabs, .settings__tabs .tabs").find(".tab").removeClass("active");
+    $(".product-one__tabs, .settings__tabs")
+      .find(".tab-item")
+      .removeClass("active-tab")
+      .hide();
+    $(".product-one__tabs .tabs, .settings__tabs .tabs")
+      .find(".tab")
+      .removeClass("active");
     $(this).addClass("active");
     $("#" + id)
       .addClass("active-tab")
